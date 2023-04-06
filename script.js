4446dc594430abf0715e3b44351423bca5a4d584
Laurie Fish <iamlauriefish@gmail.com>
2023-04-06 15:24:33 -0700

    commented on JS and HTML

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 var currentData = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
 var currentHour = dayjs().hour();
 
 $('#currentDay').text(currentData);
-
+//when clicked, DOM elements will be used to show the item and time
 $('.saveBtn').on('click', function () {
   console.log(this);
   var text = $(this).siblings('.description').val();
@@ -13,15 +11,15 @@ $('.saveBtn').on('click', function () {
 
   localStorage.setItem(time, text);
 });
-
+//grabbing DOM elements for the time of each block
 function timeTracker() {
   $('.time-block').each(function () {
     var timeBlockId = $(this).attr('id');
     var plannerHour = timeBlockId.split('hour')[1];
     var existingNote = localStorage.getItem(timeBlockId);
     //console.log(existingNote);
-    console.log(plannerHour, currentHour);
 
+    //finding out which color the line should change to in regard to past, present and future times
     if (plannerHour < currentHour) {
       $(this).addClass('past');
     } else if (plannerHour == currentHour) {
@@ -44,9 +42,5 @@ $('#hour4pm .description').val(localStorage.getItem('hour4pm'));
 $('#hour5pm .description').val(localStorage.getItem('hour5pm'));
 
 timeTracker();
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
 
